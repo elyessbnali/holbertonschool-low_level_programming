0d0abe67a0519e8104a3298854bce1b4da590dae
elyessbnali <elyess.ouni11@gmail.com>
2023-10-18 16:09:54 +0200

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * File: 9-print_comb.c
+ * Auth: Brennan D Baraban
+ */
+int _putchar(char c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
