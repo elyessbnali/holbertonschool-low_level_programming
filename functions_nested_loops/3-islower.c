c0d68d0765a7fad82ae9f96ab9b638cae6cec77e
elyessbnali <elyess.ouni11@gmail.com>
2023-10-18 17:26:06 +0200

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - prints _putchar
+ *
+ * Return: Always (Success)
+ */
+int _islower(int c)
+{
+	if (c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
