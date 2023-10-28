a31b879ec275ab24dc880722ec8c773e166b6120
elyessbnali <elyess.ouni11@gmail.com>
2023-10-28 20:05:22 +0200

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * string_toupper - function that reverses a string.
+ * Return: upp
+ * @str : pointeur
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (int)str[i] - 32;
+	}
+	return (str);
+}
